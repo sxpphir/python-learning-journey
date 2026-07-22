9af410b2f3d28ace7c9082415c4898d1df1834ec
Sapphir32 <sxpphire.ux@gmail.com>
2026-07-22 16:35:52 -0600

    Add initial Python exercises and practice scripts

---
+++ Exercises/exercise2.py
@@ -0,0 +1,8 @@
+name = 'Ana'
+age = 25
+name_and_age = f'My name is {name} and Im {age} years old'
+print(name_and_age)
+
+num1 = 36
+num2 = 33
+print(f'The sum of {num1} and {num2} is {num1 + num2}')
