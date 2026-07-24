1eb02de3926dfb6b918a11106f89c4bc8e11022a
Sapphir32 <sxpphire.ux@gmail.com>
2026-07-23 19:59:14 -0600

    fix: update f-string text in examples

--- Exercises/exercise2.py
+++ Exercises/exercise2.py
@@ -1,7 +1,11 @@
+# Example 1: Presentation
+
 name = 'Ana'
 age = 25
-name_and_age = f'My name is {name} and Im {age} years old'
+name_and_age = f"My name is {name} and I'm {age} years old"
 print(name_and_age)
+
+# Example 2: Sum of two numbers
 
 num1 = 36
 num2 = 33
